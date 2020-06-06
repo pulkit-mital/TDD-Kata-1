77344351c75291e5cde13f3f7f58d5ec138c86bd
viper <pulkit.mital@gmail.com>
2020-06-07 03:15:10 +0530

    <refactor>(code): refactor code

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,16 +2,10 @@ import java.util.Arrays;
 
 public class StringCalculator {
     public int Add(String numbers) {
-        if (numbers == null || numbers.isEmpty()) {
-            return 0;
-        } else {
-            int[] integerNumbers = StringUtils.splitAndGetListOfIntegerNumbers(numbers, ",|\n");
-            int[] negativeNumbers = Arrays.stream(integerNumbers).filter(x -> x < 0).toArray();
-            if(negativeNumbers.length > 0){
-                throw new RuntimeException("Negatives not allowed: -1");
-            }
-            return Calculator.sum(integerNumbers);
-        }
+        int[] integerNumbers = StringUtils.splitAndGetListOfIntegerNumbers(numbers);
+        StringUtils.ensureAllNonNegatives(integerNumbers);
+        return Calculator.sum(integerNumbers);
+
     }
 
 }
--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,26 +12,36 @@ public class StringUtils {
         return numbers.split(regex);
     }
 
-    public static int[] splitAndGetListOfIntegerNumbers(String numbers, String regex) {
+    public static int[] splitAndGetListOfIntegerNumbers(String numbers) {
         String[] numberList = splitUsingCustomDelimiterOrByCommaAndNewLine(numbers);
         int[] integerNumbers = new int[numberList.length];
         for (int i = 0; i < numberList.length; i++) {
             integerNumbers[i] = convertStringToInteger(numberList[i]);
         }
-
         return integerNumbers;
     }
 
-    public static String[] splitUsingCustomDelimiterOrByCommaAndNewLine(String numbers){
-        if(numbers.startsWith("//")) {
+    public static String[] splitUsingCustomDelimiterOrByCommaAndNewLine(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return new String[0];
+        }else if (numbers.startsWith("//")) {
             Matcher matcher = Pattern.compile("//(.*)\n(.*)").matcher(numbers);
             matcher.matches();
             String customDelimiter = matcher.group(1);
             String numbersList = matcher.group(2);
-            return getListOfNumbers(numbersList, Pattern.quote(customDelimiter)+"|\n");
-        }else{
-            return getListOfNumbers(numbers,",|\n");
+            return getListOfNumbers(numbersList, Pattern.quote(customDelimiter) + "|\n");
+        } else {
+            return getListOfNumbers(numbers, ",|\n");
         }
     }
+
+    public static void ensureAllNonNegatives(int[] numbers) {
+        int[] negativeNumbers = Arrays.stream(numbers).filter(x -> x < 0).toArray();
+        if (negativeNumbers.length > 0) {
+            throw new RuntimeException("Negatives not allowed: -1");
+        }
+    }
+
+
 }
 
